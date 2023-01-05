ec2581d5d370335f56b70da0caebf1cb50a53952
venkat <venkat.mobiqa@gmail.com>
2023-01-05 19:11:27 +0530

    create servers scripts

--- catalogue.sh
+++ catalogue.sh
@@ -20,5 +20,5 @@ systemctl start catalogue
 
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
-mongo --host localhost </app/schema/catalogue.js
+mongo --host mongodb-dev.mobiqa.online </app/schema/catalogue.js
 
