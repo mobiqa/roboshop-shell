317371cfbd4d01f627bbd9445d37fe8a573db037
venkat <venkat.mobiqa@gmail.com>
2023-01-05 19:29:40 +0530

    create servers scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,13 @@
 script_location=$(pwd)
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-#useradd roboshop
+useradd roboshop
 mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-
 rm -rf /app/*
-
 cd /app
 unzip /tmp/catalogue.zip
-
 cd /app
 npm install
 
@@ -18,7 +16,7 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
-mongo --host mongodb-dev.mobiqa.online </app/schema/catalogue.js
 
+mongo --host mongodb-dev.mobiqa.online </app/schema/catalogue.js
