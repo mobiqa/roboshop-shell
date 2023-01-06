f7f4cca1e6be672d3c8ff463042370300caafedb
venkat <venkat.mobiqa@gmail.com>
2023-01-06 13:49:38 +0530

    create servers scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,64 @@
-script_location=$(pwd)
+source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir -p /app
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
+print_head "Configuring NodeJS Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
+
+print_head "Install NodeJS"
+yum install nodejs -y &>>${LOG}
+status_check
+
+print_head "Add Application User"
+id roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${LOG}
+fi
+status_check
+
+mkdir -p /app &>>${LOG}
+
+print_head "Downloading App content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
+status_check
+
+print_head "Cleanup Old Content"
+rm -rf /app/* &>>${LOG}
+status_check
+
+print_head "Extracting App Content"
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
+unzip /tmp/catalogue.zip &>>${LOG}
+status_check
 
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "Installing NodeJS Dependencies"
+cd /app &>>${LOG}
+npm install &>>${LOG}
+status_check
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "Configuring Catalogue Service File"
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
+status_check
 
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${LOG}
+status_check
+
+print_head "Enable Catalogue Service "
+systemctl enable catalogue &>>${LOG}
+status_check
+
+print_head "Start Catalogue service "
+systemctl start catalogue &>>${LOG}
+status_check
+
+print_head "Configuring Mongo Repo "
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+status_check
+
+print_head "Install Mongo Client"
+yum install mongodb-org-shell -y &>>${LOG}
+status_check
+
+print_head "Load Schema"
 mongo --host mongodb-dev.mobiqa.online </app/schema/catalogue.js
+status_check
