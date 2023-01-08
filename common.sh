94e35edc74fa33e5fb940595a3d776d957068e32
venkat <venkat.mobiqa@gmail.com>
2023-01-08 14:39:07 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -72,7 +72,7 @@ LOAD_SCHEMA() {
       status_check
 
       print_head "Load Schema"
-      mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
+      mongo --host mongodb-dev.mobiqa.online </app/schema/${component}.js &>>${LOG}
       status_check
     fi
 
@@ -83,7 +83,7 @@ LOAD_SCHEMA() {
       status_check
 
       print_head "Load Schema"
-      mysql -h mysql-dev.devopsb70.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+      mysql -h mysql-dev.mobiqa.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
       status_check
     fi
 
