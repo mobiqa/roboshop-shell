c69ea9b82631068b3ff441c62b0d3449cf70dca2
venkat <venkat.mobiqa@gmail.com>
2023-01-09 17:03:47 +0530

    updating ./roboshop-shell

---
+++ rabbitmq.sh
@@ -0,0 +1,40 @@
+source common.sh
+
+
+if [ -z "${root_mysql_password}" ]; then
+  echo "Variable root_mysql_password is missing"
+  exit
+fi
+
+
+print_head "Configuring Erlang yum repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
+status_check
+
+print_head "Configuring Rabbitmq yum repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
+status_check
+
+print_head "Install Rabbitmq and Erlang"
+yum install erlang rabbitmq-server -y &>>${LOG}
+status_check
+
+print_head "Enable Rabbitmq"
+systemctl enable rabbitmq-server &>>${LOG}
+status_check
+
+print_head "Start Rabbitmq"
+systemctl start rabbitmq-server &>>${LOG}
+status_check
+
+print_head "Add Application user"
+rabbitmqctl add_user roboshop roboshop123 &>>${LOG}
+status_check
+
+print_head "Add permission to Application user"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
+status_check
+
+print_head "Install Rabbitmq"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+status_check
