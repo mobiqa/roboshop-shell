740f275844b91a0487c7418e2f1484f01e1ee577
venkat <venkat.mobiqa@gmail.com>
2023-05-06 20:36:54 +0530

    updating ./roboshop-shell

--- create-servers.sh
+++ create-servers.sh
@@ -43,12 +43,12 @@ if [ -z "${SGID}" ]; then
 fi
 
 
-#for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-#  COMPONENT="${component}-${env}"
-#  create_ec2
-#done
-
-for component in cart user shipping payment mysql rabbitmq redis; do
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
   COMPONENT="${component}-${env}"
   create_ec2
 done
+
+#for component in cart user shipping payment mysql rabbitmq redis; do
+#  COMPONENT="${component}-${env}"
+#  create_ec2
+#done
